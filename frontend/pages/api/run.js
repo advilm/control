86cb22477e1eba25f75c1a5bb8ed6e94410bad6a
Adil Mohiuddin <amohi9046@gmail.com>
2022-03-19 22:41:54 -0700

    rename button

--- frontend/pages/api/run.js
+++ frontend/pages/api/run.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
 	if (['up', 'down', 'left', 'right', 'click'].includes(req.query.q)) {
-		fetch('http://localhost:3000/run?command=' + req.query.q, {method: 'POST'});
+		fetch('http://localhost:9000/run?command=' + req.query.q, {method: 'POST'});
 	}
 	res.status(200).json({});
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,14 +3,7 @@ import { MantineProvider } from '@mantine/core';
 import { Button, Grid, Container } from '@mantine/core';
 export default function App() {
 	return (
-		<MantineProvider
-			theme={{
-				// Override any other properties from default theme
-				// fontFamily: 'Open Sans, sans serif',
-				// spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
-				colorScheme: 'dark',
-			}}
-		>
+		<MantineProvider theme={{ colorScheme: 'dark' }}>
 			<Container size={300}>
 				<Grid columns={3}>
 					<Grid.Col span={1}></Grid.Col>
@@ -20,9 +13,9 @@ export default function App() {
 					<Grid.Col span={1}></Grid.Col>
 					<Grid.Col span={1}>
 						<Button fullWidth='50' onClick={() => fetch('/api/run?q=left')}>Left</Button>
-					</Grid.Col>	
+					</Grid.Col>
 					<Grid.Col span={1}>
-						<Button fullWidth='50' onClick={() => fetch('/api/run?q=click')}>Left</Button>
+						<Button fullWidth='50' onClick={() => fetch('/api/run?q=click')}>Click</Button>
 					</Grid.Col>
 					<Grid.Col span={1}>
 						<Button fullWidth='50' onClick={() => fetch('/api/run?q=right')}>Right</Button>
