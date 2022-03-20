0587941adc68e56c0e2e94a32f2d2ea1cdf0d95c
Adil Mohiuddin <amohi9046@gmail.com>
2022-03-19 17:30:56 -0700

    fix other panic

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,46 +10,48 @@ async fn main() {
     let (_, read) = ws_stream.split();
 
     read.for_each(|data| async {
-        if let Ok(message) = data.unwrap().into_text() {
-            match &message[..] {
-                "up" => {
-                    println!("xdotool key Up");
-                    tokio::process::Command::new("xdotool")
-                        .arg("mousemove_relative")
-                        .arg("0")
-                        .arg("-100")
-                        .spawn()
-                        .expect("Failed to execute process");
+        if !data.is_err() {
+            if let Ok(message) = data.unwrap().into_text() {
+                match &message[..] {
+                    "up" => {
+                        println!("xdotool key Up");
+                        tokio::process::Command::new("xdotool")
+                            .arg("mousemove_relative")
+                            .arg("0")
+                            .arg("-100")
+                            .spawn()
+                            .expect("Failed to execute process");
+                    }
+                    "down" => {
+                        println!("xdotool key Down");
+                        tokio::process::Command::new("xdotool")
+                            .arg("mousemove_relative")
+                            .arg("0")
+                            .arg("100")
+                            .spawn()
+                            .expect("Failed to execute process");
+                    }
+                    "left" => {
+                        println!("xdotool key Left");
+                        tokio::process::Command::new("xdotool")
+                            .arg("mousemove_relative")
+                            .arg("--")
+                            .arg("-100")
+                            .arg("0")
+                            .spawn()
+                            .expect("Failed to execute process");
+                    }
+                    "right" => {
+                        println!("xdotool key Right");
+                        tokio::process::Command::new("xdotool")
+                            .arg("mousemove_relative")
+                            .arg("100")
+                            .arg("0")
+                            .spawn()
+                            .expect("Failed to execute process");
+                    }
+                    _ => {}
                 }
-                "down" => {
-                    println!("xdotool key Down");
-                    tokio::process::Command::new("xdotool")
-                        .arg("mousemove_relative")
-                        .arg("0")
-                        .arg("100")
-                        .spawn()
-                        .expect("Failed to execute process");
-                }
-                "left" => {
-                    println!("xdotool key Left");
-                    tokio::process::Command::new("xdotool")
-                        .arg("mousemove_relative")
-                        .arg("--")
-                        .arg("-100")
-                        .arg("0")
-                        .spawn()
-                        .expect("Failed to execute process");
-                }
-                "right" => {
-                    println!("xdotool key Right");
-                    tokio::process::Command::new("xdotool")
-                        .arg("mousemove_relative")
-                        .arg("100")
-                        .arg("0")
-                        .spawn()
-                        .expect("Failed to execute process");
-                }
-                _ => {},
             }
         }
     })
