1863fe86e9bd88f305d893f3b2a27778a67690a4
Adil Mohiuddin <amohi9046@gmail.com>
2022-03-22 17:34:59 -0700

    refactor web

--- frontend/pages/api/run.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-	if (['up', 'down', 'left', 'right', 'click'].includes(req.query.q)) {
-		fetch('http://localhost:9000/run?command=' + req.query.q, {method: 'POST'});
-	}
-	res.status(200).json({});
-}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,27 +2,29 @@ import React from 'react';
 import { MantineProvider } from '@mantine/core';
 import { Button, Grid, Container } from '@mantine/core';
 export default function App() {
+	const run = cmd => fetch(`/run?command=${cmd}`, { method: 'POST' });
+
 	return (
 		<MantineProvider theme={{ colorScheme: 'dark' }}>
 			<Container size={300}>
 				<Grid columns={3}>
 					<Grid.Col span={1}></Grid.Col>
 					<Grid.Col span={1}>
-						<Button fullWidth='50' onClick={() => fetch('/api/run?q=up')}>Up</Button>
+						<Button fullWidth='50' onClick={() => run('up')}>Up</Button>
 					</Grid.Col>
 					<Grid.Col span={1}></Grid.Col>
 					<Grid.Col span={1}>
-						<Button fullWidth='50' onClick={() => fetch('/api/run?q=left')}>Left</Button>
+						<Button fullWidth='50' onClick={() => run('left')}>Left</Button>
 					</Grid.Col>
 					<Grid.Col span={1}>
-						<Button fullWidth='50' onClick={() => fetch('/api/run?q=click')}>Click</Button>
+						<Button fullWidth='50' onClick={() => run('click')}>Click</Button>
 					</Grid.Col>
 					<Grid.Col span={1}>
-						<Button fullWidth='50' onClick={() => fetch('/api/run?q=right')}>Right</Button>
+						<Button fullWidth='50' onClick={() => run('right')}>Right</Button>
 					</Grid.Col>
 					<Grid.Col span={1}></Grid.Col>
 					<Grid.Col span={1}>
-						<Button fullWidth='50' onClick={() => fetch('/api/run?q=down')}>Down</Button>
+						<Button fullWidth='50' onClick={() => run('down')}>Down</Button>
 					</Grid.Col>
 				</Grid>
 			</Container>
